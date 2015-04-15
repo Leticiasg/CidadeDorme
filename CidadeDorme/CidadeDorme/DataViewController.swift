6be9bbac20fc2505671d7c4f5bc1c4be919f26d2
Leticiasg <leticiagoncalves2005@gmail.com>
2015-04-15 13:46:48 -0300

    MVC Implemented!

--- CidadeDorme/CidadeDorme/DataViewController.swift
+++ CidadeDorme/CidadeDorme/DataViewController.swift
@@ -18,6 +18,7 @@ class DataViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
